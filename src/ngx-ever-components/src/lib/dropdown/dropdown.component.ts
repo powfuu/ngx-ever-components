4b0e44bd70067f4b173f0736a8b58444b4bf7df0
powfuu <everitjhon@gmail.com>
2024-05-18 21:43:54 +0200

    fix dropdown

--- src/ngx-ever-components/src/lib/dropdown/dropdown.component.ts
+++ src/ngx-ever-components/src/lib/dropdown/dropdown.component.ts
@@ -11,6 +11,12 @@ export class DropdownComponent {
   @Input() items: string[] = [];
   @Input() clickAction!: (item: string) => void;
 
+  closeDropdown(): void {
+    if (this.showDropdown) {
+      this.showDropdown = false;
+    }
+  }
+
   onItemClick(item: string) {
     if (this.clickAction) {
       this.clickAction(item);
